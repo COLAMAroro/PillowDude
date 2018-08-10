c0da8f256ee88e4881402d6560e275c0785bd0dd
Crispin Suminski <suminski.crispin@epitech.eu>
2018-08-10 23:42:33 +0200

    Using queue instead of list, only once in queue can one user be

--- query.py
+++ query.py
@@ -2,12 +2,13 @@
 
 import discord
 import asyncio
+import queue
 from time import sleep
 from tkn import *
 
 client = discord.Client()
 
-queue = []
+user_queue = queue.Queue(10)
 
 @client.event
 @asyncio.coroutine
@@ -16,11 +17,13 @@ def on_message(message):
         return
 
     if (message.content.startswith("!queue")):
-        yield from client.send_message(message.channel, message.author.mention + " joined the queue in " + str(len(queue)) + "th position")
-        queue.append(message.author)
+        if (message.author not in user_queue.queue):
+            yield from client.send_message(message.channel, message.author.mention + " joined the queue in " + str(len(user_queue.queue)) + "th position")
+            user_queue.put(message.author)
+        else:
+            yield from client.send_message(message.channel, "You are already in queue!")
 
     if (message.content.startswith("!skip")):
-        yield from client.send_message(message.channel, queue[0].mention + " was skipped")
-        queue.pop(0)
+        yield from client.send_message(message.channel, user_queue.get_nowait().mention + " was skipped")
 
 client.run(token)
